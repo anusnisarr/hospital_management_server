0158e012e9b163777a0983bd535046484cf3145b
Anas Nisar <anusnisarr@gmail.com>
2025-12-17 23:53:55 +0500

    cors origin

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(
     origin: (origin, cb) => {
       const whitelist = [
         "https://clinic-management-awn6.onrender.com",
-        "hospital-management-3y1x.onrender.com",
+        "https://hospital-management-3y1x.onrender.com",
         "http://localhost:5173"
       ];
       if (!origin || whitelist.includes(origin)) {
