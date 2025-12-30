c43823b331c065ecd3f0d39c772917543ad3730c
Anas Nisar <anusnisarr@gmail.com>
2025-12-30 09:26:11 +0500

    fix cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: (origin, cb) => {
       const whitelist = [
-        "https://clinic-management-awn6.onrender.com",
+        "https://hospital-management-3y1x.onrender.com",
         "http://localhost:5173"
       ];
       if (!origin || whitelist.includes(origin)) {
